f0c906cf204e9f07dfdc926f6e483dc2dc4da79c
yasin.demirkan <yasin.demirkan@CPX-HO9LJVP82KC.dir.svc.accenture.com>
2020-02-04 14:43:03 +0100

    stuff, use it everyone, happy days!!!!

--- src/main/java/com/acn/powerrangers/dayiversary/models/User.java
+++ src/main/java/com/acn/powerrangers/dayiversary/models/User.java
@@ -2,12 +2,22 @@ package com.acn.powerrangers.dayiversary.models;
 
 import java.time.LocalDate;
 
+import javax.annotation.Generated;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class User {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 	private String firstName;
 	private String lastName;
 	private LocalDate birthDate;
+	
 	
 	public User() {
 		
